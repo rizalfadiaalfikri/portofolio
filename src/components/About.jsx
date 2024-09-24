480f653af10f9264262d3b09db4182b32629df99
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2024-09-24 22:16:31 +0700

    feat(about): add eduction description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     const [activeTab, setActiveTab] = useState(0);
 
     return <>
-        <div className="bg-gray-800 text-white font-jetbrains w-full">
+        <div className="bg-gray-800 text-white font-jetbrains w-full pb-20">
             <div className="container mx-auto flex justify-center items-center flex-col mt-10">
                 <p className="text-justify text-lg">I am a Backend Developer with approximately 2 years of experience, specializing in Java and Spring Boot. I have developed efficient backend applications and collaborated with teams to create innovative software solutions. Additionally, I have a strong interest in Artificial Intelligence, particularly in Computer Vision and Natural Language Processing, and I continuously enhance my knowledge in these areas. I believe my combination of backend expertise and interest in AI allows me to make meaningful contributions to the projects I work on.</p>
                 <div className="mt-5 w-full">
@@ -49,9 +49,6 @@ const About = () => {
                                     </section>
                                 </div>
                             </div>
-
-                            
-                            
 
                             <div className="flex justify-start mt-5">
                                 <div className="w-3/4">
@@ -110,8 +107,23 @@ const About = () => {
                         )}
                         {activeTab === 1 && (
                         <div>
-                            <h2 className="text-2xl font-bold">Education</h2>
-                            <p>Here is the content for Education.</p>
+                            <div className="flex justify-start mt-5">
+                                <div className="w-3/4">
+                                    <h2 className="text-2xl font-bold bg-gradient-to-r from-white to-blue-500 bg-clip-text text-transparent">PIKSI GANESHA POLYTECHNIC</h2>
+
+                                    <section className="mt-5 relative pl-10">
+                                        <div className="absolute left-0 top-0 h-full border-l-4 border-gray-400"></div> {/* Garis di samping kiri */}
+                                        {/* Lingkaran di ujung atas */}
+                                        <div className="absolute left-[-6px] top-0 h-4 w-4 rounded-full bg-gray-400"></div>
+                                        
+                                        {/* Lingkaran di ujung bawah */}
+                                        <div className="absolute left-[-6px] bottom-0 h-4 w-4 rounded-full bg-gray-400"></div>
+                                        <p className="text-gray-100 mb-1">Information Systems</p>
+                                        <p className="text-gray-400 mb-4">2019 - 2024</p>
+                                        <p className="text-justify">I completed a D4 degree in Information Systems at Piksi Polytechnic for approximately 4 years with a GPA of 3.50. I published a scientific journal entitled  <a href="https://jurnal.univrab.ac.id/index.php/rabit/article/view/4067" target="_blank"><span className="italic font-semibold">&quot;IMPLEMENTATION OF THE MODEL VIEW CONTROLLER PRINCIPLE IN A WEB-BASED FACULTY INFORMATION SYSTEM.&quot;</span></a></p>
+                                    </section>
+                                </div>
+                            </div>
                         </div>
                         )}
                     </div>
