8f234e58d255bb7cf74d8b12a5a7ef23164ca6e7
rizalfadiaalfikri <rizalfadiaalfikri859@gmail.com>
2024-09-25 10:02:13 +0700

    feat(footer): add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About"
 import Blog from "./components/Blog"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 import Home from "./components/Home"
 import Nabvar from "./components/Navbar"
 import Project from "./components/Project"
@@ -29,6 +30,7 @@ function App() {
         <Contact/>
       </Title>
 
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return <>
+        <div className="bg-gray-800 text-white font-jetbrains w-full box-border">
+            <div className="container mx-auto flex justify-center items-center flex-col">
+                <div className="flex justify-between items-center w-full">
+                    <div className="flex justify-center items-center w-full p-5 text-center">
+                    <span className="text-sm text-white sm:text-center dark:text-gray-400 font-semibold">
+                        © 2024 <a href="https://flowbite.com/" className="hover:underline">Rizal Fadia Al Fikri™</a>. All Rights Reserved.
+                    </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+}
+
+export default Footer;
